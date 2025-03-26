50f191d1ef1969efee79204db4ad42a5a02fdfaa
Rizone Hasan <reznuhasan31@gmail.com>
2025-03-26 15:58:16 +0600

    rating part make dynamic done

--- app/components/superItems.jsx
+++ app/components/superItems.jsx
@@ -1,9 +1,13 @@
 import Image from "next/image"
-import Rating from "./rating"
+import Rating from "./fullRating"
+import FullRating from "./fullRating";
+import DynamicRating from "./dynamicRating";
 
 export default function SuperItems({ item }) {
     const { cooking_time, rating, thumbnail, title } = item
-    // const ratings=Array.fill(rating)
+    const {average_rating}=rating;
+    const fullRatingValue=Math.floor(average_rating);
+    const emptyRating=(average_rating-fullRatingValue)*10;
     return (
         <div>
             <Image
@@ -15,7 +19,8 @@ export default function SuperItems({ item }) {
             />
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
             <div className="flex items-center text-yellow-500 mb-2">
-                <Rating />
+                {[...Array(fullRatingValue)].map((_,index)=><FullRating key={index}/>)}
+                {<DynamicRating value={emptyRating}/>}
             </div>
             <p className="text-gray-600">{cooking_time}</p>
         </div>
