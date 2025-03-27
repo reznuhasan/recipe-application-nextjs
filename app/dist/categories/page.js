cc797bfe8391fb8d20f3b448d5787cc527182033
Rizone Hasan <reznuhasan31@gmail.com>
2025-03-27 17:15:59 +0600

    basic recipe app done

--- app/dist/categories/page.js
+++ app/dist/categories/page.js
@@ -1,8 +1,17 @@
+import CategoryItem from "@/app/components/categoryItem";
+import categoriesData from "@/app/data/categories";
 
 export default function Categories() {
   return (
-    <div>
-      Categories Page
+    <div className="container mx-auto px-4 py-8 mt-[100px]">
+      <h1 className="text-5xl font-bold mb-12">Categories</h1>
+      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8">
+        {
+          categoriesData.map(category=><CategoryItem key={category.id} category={category}/>)
+        }
+      </div>
+
+
     </div>
   )
 }
--- app/dist/latest-recipes/page.js
+++ app/dist/latest-recipes/page.js
@@ -1,8 +1,28 @@
+import LatestItem from "@/app/components/latestItem";
+import recipes from "@/app/data/recipes";
+import Link from "next/link";
 
 export default function LatestRecipes() {
   return (
-    <div>
-      LatestRecipes
+    <div className="container mx-auto px-4 py-8 mt-[100px]">
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <h1 className="text-4xl font-bold mb-2">
+            Desserts <span className="text-gray-500 text-2xl font-normal">(98 Recipes)</span>
+          </h1>
+          <p className="text-gray-600">
+            One thing I learned living in the Canarsie section of Brooklyn, NY was how to cook a good Italian meal. Here
+            is a recipe I created after having this dish in a restaurant. Enjoy!
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        {recipes.map(recipe=>
+        <Link href={`/dist/recipes/${recipe.title}`} key={recipe.title}>
+        <LatestItem value={recipe}/>
+        </Link>
+        )}
+      </div>
     </div>
   )
 }
